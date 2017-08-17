17c0bdaf2f85f14b51adab51fe75259f0b00ccbb
Dumrus Sukasam <dumrus.sukasam@unicity.com>
2017-08-17 16:09:08 +0700

    Update class SDK.

--- bot.php
+++ bot.php
@@ -1,49 +1,22 @@
 <?php
 
-include_once("vendor/autoload.php");
+include ('line-bot.php');
 
-$channelToken = "YNjsjurrJe8/74ZkrudrQCFjyWiWcujuzzhAMZ/fden0FBNYAfT+2Q6qAIzBrVDN273QKEVlUinjTeNPh+6696J4NBEPF34LaT8GfExE118m0I8sfZfdfr4jgd0g8H0viC+Sfk15MmzWbtODBuS0UwdB04t89/1O/w1cDnyilFU=";
-$channelSecret = "0607c7cf2e8d92697898857ff87a69b5";
+$channelSecret = '0607c7cf2e8d92697898857ff87a69b5';
+$access_token  = 'YNjsjurrJe8/74ZkrudrQCFjyWiWcujuzzhAMZ/fden0FBNYAfT+2Q6qAIzBrVDN273QKEVlUinjTeNPh+6696J4NBEPF34LaT8GfExE118m0I8sfZfdfr4jgd0g8H0viC+Sfk15MmzWbtODBuS0UwdB04t89/1O/w1cDnyilFU=';
 
-
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelToken);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			
-			$userId = $event['source']['userId'];
-			
-			if($text == "สวัสดี" || $text == "Hello"){
-				
-				$response = $bot->getProfile($userId);
-				if ($response->isSucceeded()) {
-					$profile = $response->getJSONDecodedBody();
-					$response = $bot->replyText($replyToken, $profile['displayName']);
-				}else{
-					$response = $bot->replyText($replyToken, $text);
-				}
-				
-			}else{
-				$response = $bot->replyText($replyToken, $text);
-			}
-			
-			
-		}
-	}
+$bot = new BOT_API($channelSecret, $access_token);
+	
+if (!empty($bot->isEvents)) {
+		
+    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+    if ($bot->isSuccess()) {
+        echo 'Succeeded!';
+        exit();
+    }
+    // Failed
+    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+    exit();
 }
 echo "OK";
 ?>
