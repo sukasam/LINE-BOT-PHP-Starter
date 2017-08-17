f211012feb9a5a85487c2cf0a3fa09ff146c3bcd
Mr.Dumrus Sukasam <dumrus.s@unicity.com>
2017-08-17 16:15:47 +0700

    change channelSecret and access_token

--- bot.php
+++ bot.php
@@ -2,8 +2,8 @@
 
 include ('line-bot.php');
 
-$channelSecret = '0607c7cf2e8d92697898857ff87a69b5';
-$access_token  = 'YNjsjurrJe8/74ZkrudrQCFjyWiWcujuzzhAMZ/fden0FBNYAfT+2Q6qAIzBrVDN273QKEVlUinjTeNPh+6696J4NBEPF34LaT8GfExE118m0I8sfZfdfr4jgd0g8H0viC+Sfk15MmzWbtODBuS0UwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '0607..........69b5';
+$access_token  = 'YNjsjurrJe.......w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
