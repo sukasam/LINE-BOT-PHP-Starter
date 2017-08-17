74e23c98900cde067323d8064c0f5b0a3d88e865
Dumrus Sukasam <dumrus.sukasam@unicity.com>
2017-08-17 13:02:26 +0700

    Include vendor/autoload.php

--- bot.php
+++ bot.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once("vendor/autoload.php");
+
 $channelToken = "YNjsjurrJe8/74ZkrudrQCFjyWiWcujuzzhAMZ/fden0FBNYAfT+2Q6qAIzBrVDN273QKEVlUinjTeNPh+6696J4NBEPF34LaT8GfExE118m0I8sfZfdfr4jgd0g8H0viC+Sfk15MmzWbtODBuS0UwdB04t89/1O/w1cDnyilFU=";
 $channelSecret = "0607c7cf2e8d92697898857ff87a69b5";
 
@@ -22,7 +24,7 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-
+			
 			/*// Build message to reply back
 			$messages = [
 				'type' => 'text',
